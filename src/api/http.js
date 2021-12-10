00d4e670d26db3d4c3f4417746bbaabab5353a66
BK201-Drama <1243532812@qq.com>
2021-12-10 12:30:31 +0800

    master

--- src/api/http.js
+++ src/api/http.js
@@ -6,12 +6,12 @@ axios.defaults.baseURL = 'http://106.55.188.184:8080/ssmBillBook/'
 // 是否为登录状态，不是就来登录获得token
 // 请求拦截
 axios.interceptors.request.use(config => {
-  console.log("发请求的拦截")
+  // console.log("发请求的拦截")
   return config
 })
 // 响应拦截
 axios.interceptors.response.use(config => {
-  console.log("接收请求的拦截")
+  // console.log("接收请求的拦截")
   // console.log(config.headers.SetCookies)
   return config
 })
--- src/pages/PageContainer/Booking/Booking.jsx
+++ src/pages/PageContainer/Booking/Booking.jsx
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import { Icon, Tabs, Grid, NumberKeyboard, Image, Field } from 'react-vant'
+import { Icon, Tabs, Grid, NumberKeyboard, Image, Field, DatetimePicker, Dialog } from 'react-vant'
 import * as BookingAPI from '../../../api/Booking/BookingAPI'
 import store from '../../../redux/store'
 import transPic from '../../../utils/transPic'
 
 export default function Booking () {
 
+  // 登录信息
   const res = store.getState()
 
+  // 存储记账类型，为支出还是收入
+  const [tab, setTab] = useState(false)
+
+  // 存储选择了哪一条数据
   const [selectedItem, setSelectedItem] = useState()
 
   // console.log(res)
 
+  // 存储支出和收入的数据
   const [listOut, setListOut] = useState([])
   const [listIn, setListIn] = useState([])
+
+  // 存储控制Popup弹出的数据
+  const [popup, setPopup] = useState(false)
 
   useEffect(async () => {
     var dat = await BookingAPI.getList(res.id)
@@ -21,12 +30,24 @@ export default function Booking () {
     await setListIn(dat.inSortlis)
   }, [])
 
+  // 控制数字键盘
   const [keyBoardVisible, setKeyBoardVisible] = useState(false)
   const [costValue, setCostValue] = useState('0.00')
 
+  // 控制时间选择器
+  const [time, setTime] = useState(1639109113)
+
   return (
     <>
-      <Tabs active="active">
+      <Tabs active="active"
+        onChange={(v) => {
+          if (v === 0) {
+            setTab(false)
+          } else {
+            setTab(true)
+          }
+        }}
+      >
         <Tabs.TabPane title="支出">
           <Grid border={false} columnNum={5}>
             {
@@ -44,7 +65,7 @@ export default function Booking () {
                     setSelectedItem(item)
                     setKeyBoardVisible(true)
                   }}
-                  hideOnClickOutside={true}
+                  // hideOnClickOutside={true}
                 />
               })
             }
@@ -53,17 +74,23 @@ export default function Booking () {
         </Tabs.TabPane>
 
         <Tabs.TabPane title="收入">
-
+          
         <Grid border={false} columnNum={5}>
           {
             listIn.map((item) => {
               const name_ = `${item.sortName}`
+              const src_ = `http://106.55.188.184:8080/ssmBillBook/upload/typeList/${transPic(item.sortImg)}`
+
               return <Grid.Item 
                 text={name_} 
                 key={name_}
-                // icon={<Image src={require(`../../../assets/img/typeList/${transPic(item.sortImg)}`).default}/>}
+                icon={<Image src={src_}/>}
                 
-                onClick={() => {console.log(456)}}
+                onClick={async () => {
+                  console.log(item)
+                  setSelectedItem(item)
+                  setKeyBoardVisible(true)
+                }}
               />
             })
           }
@@ -72,30 +99,71 @@ export default function Booking () {
         </Tabs.TabPane>
       </Tabs>
 
+      {/* 数字键盘 */}
       <NumberKeyboard 
-        title={<Field label="  " readonly value={costValue}/>}
+        title={<Field label="" readonly value={costValue}/>}
         value={costValue}
-        theme="custom" 
+        theme="custom"
         extraKey={['.', <Icon name="calendar-o"/>]}
         closeButtonText="完成"
         visible={keyBoardVisible}
-        onInput={(v) => {
+        onInput={async (v) => {
 
           if(typeof v !== 'string' && typeof v !== 'number') {
             console.log('你是日历')
+            const tmp = costValue
+            await setPopup(true)
+            await setKeyBoardVisible(false)
+
+            // 避免日历对象也被放进输入框
+            await setCostValue(tmp)
           } else {
             console.log(v)
           }
         }}
-        onClose={() => {
-          console.log(typeof costValue, costValue)
-          
+        onClose={async () => {
+          console.log(typeof res.id, res.id)
+          const re = await BookingAPI.addBook({
+            userId: res.id,
+            sortid: selectedItem.id,
+            payid: 1,
+            cost: parseFloat(costValue),
+            crdate: 1512565107000,
+            content: 'none',
+            income: tab
+          })
+          console.log('请求结果:', re)
         }}
         onChange={setCostValue}
         onBlur={async () => {
           setKeyBoardVisible(false)
         }}
       />
+
+      {/* 时间选择框 */}
+      <Dialog
+        // closeable
+        title="请选择日期"
+        round
+        visible={popup}
+        showCancelButton={false}
+        showConfirmButton={false}
+      >
+        <DatetimePicker
+          type="date"
+          minDate={new Date(2010, 0, 1)}
+          maxDate={new Date(2024, 10, 1)}
+          value={time}
+          onCancel={async () => {
+            await setPopup(false)
+            await setKeyBoardVisible(true)
+          }}
+          onConfirm={async (value) => {
+            await setTime(value)
+            await setPopup(false)
+          }}
+        />
+      </Dialog>
     </>
   )
 }
